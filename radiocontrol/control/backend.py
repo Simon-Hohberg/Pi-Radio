8740efae55adbedb84d236ef98767232f31e5d4a
Simon Hohberg <simhoh@gmx.de>
2014-08-15 16:19:19 +0200

    Refactoring backend

--- radiocontrol/control/backend.py
+++ radiocontrol/control/backend.py
@@ -1,59 +1,108 @@
-from multiprocessing.connection import Listener
-from array import array
 from control.singleton import Singleton
 from threading import Lock
 from control.controls import Controls, ControlsThread
+import control.utils as utils
 import os
 import re
 import subprocess as sub
 
 FNULL = open(os.devnull, 'w')
 VOLUME_REGEX = re.compile('Front\sLeft:\sPlayback\s\d+\s\[(\d?\d?\d)%')
-URI_REGEX = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+STATIONS_FILE = 'stations.radio'
 
 @Singleton
 class Backend():
+    '''
+    This is the singleton backend that is in charge of implementing the actual
+    input effects like switching the station, changing the volume. Also, starts
+    a ControlsThread for reading the inputs.
+    '''
+    
     
     def __init__(self):
-        
         self.mpc_lock = Lock()
         self.station_file_lock = Lock()
         self.noise_lock = Lock()
         self.amixer_lock = Lock()
         self.noise_process = None
-        self.stations = self.read_stations()
+        self.offline = False
+        
+        # check internet connection
+        packet_loss = utils.check_inet_connection()
+        
+        if packet_loss == 100:
+            self.offline = True
+        
+        # read stations from file
+        self.stations = self.read_stations(STATIONS_FILE)
+        
+        # when online create mpc playlist, otherwise do not load stations, since
+        # m3u resolution would fail, instead create ad-hoc network to all 
+        # configuration
+        if not self.offline:
+            self.load_stations(self.stations)
+        else:
+            print('No internet connection. Setting up ad-hoc network.')
+            utils.setup_ad_hoc()
+        
+        # create controls model and thread for reading input 
+        self.controls = Controls()
+        self.controls.set_update_callback(self.update)
+        controls_thread = ControlsThread(self.controls)
+        controls_thread.start()
+        
+        # start the radio when online
+        if not self.offline:
+            self.play()
+    
+    
+    def load_stations(self, stations):
+        '''
+        Creates a mpc playlist with the given stations. First the current mpc
+        playlist is cleared, then the new stations are added. Tries to resolve
+        m3u URIs automatically.
+        Thread safe, requires mpc_lock.
+        @param stations: Stations for new mps playlist 
+        '''
+        # > lock
+        self.mpc_lock.acquire()
         
         # clear playlist
         sub.call(['mpc', 'clear'])
         # add all stations from stations file
-        for s in self.stations:
+        for s in stations:
             uri = s[1]
             # mpc cannot handle m3u, workaround: parsing the m3u and using first
             # uri in list
             if uri.endswith('.m3u'):
-                m3u_data = sub.check_output(['curl', uri])
-                uri = URI_REGEX.findall(m3u_data)[0]
-            sub.call(['mpc', 'add', uri])
+                try:
+                    uri = utils.resolve_m3u(uri)
+                    sub.call(['mpc', 'add', uri])
+                except ValueError:
+                    print('Cannot resolve m3u: ' + s[1] + ' for ' + s[0] + '. Station is not added.')
+            else:
+                sub.call(['mpc', 'add', uri])
         
-        # create controls and thread for interfacing
-        self.controls = Controls()
-        self.controls.set_update_callback(self.update)
-        
-        controls_thread = ControlsThread(self.controls)
-        controls_thread.start()
-        
-        # start the radio
-        self.play()
+        # < release
+        self.mpc_lock.release()
     
     
-    def read_stations(self):
+    def read_stations(self, stations_file_name):
+        '''
+        Reads a list of stations from the given file. Each line in the file has
+        to have the form: "<readable station name>,<station uri>".
+        Thread safe, requires station_file_lock
+        @param stations_file_name: Name of the file to read stations from
+        @return: List of tuples: [ (<readable station name>, <station uri>), 
+        ... ]
+        '''
         # > lock
         self.station_file_lock.acquire()
         
         try:
-            stations_file = open('stations.radio', 'r')
+            stations_file = open(stations_file_name, 'r')
             station_list = []
-            # expecting csv station file (Station Name,URL)
+            # expecting csv station file (Station Name,URI)
             for line in stations_file:
                 splitted = line.split(',')
                 station_list.append((splitted[0].strip(), splitted[1].strip()))
@@ -67,13 +116,23 @@ class Backend():
         return station_list
     
     
-    def write_stations(self, station_list):
+    def write_stations(self, station_list, stations_file_name):
+        '''
+        Writes the list of tuples to the file with given name. List has to 
+        contain tuples like (<readable station name>, <station uri>). The tuples
+        will be written CSV like, one line for each tuple: "<readable station
+        name>,<station uri>". Overwrites old file.
+        Thread safe, requires "station_file_lock"
+        @param station_list: List of tuples with station name and URI to be 
+        written
+        @param stations_file_name: Name of the file to write to
+        '''
         self.stations = station_list
         # > lock
         self.station_file_lock.acquire()
         
         # creates file, overwrites old file
-        stations_file = open('stations.radio', 'w+')
+        stations_file = open(stations_file_name, 'w+')
         for station in station_list:
             stations_file.write(station[0] + ',' + station[1] + '\n')
         stations_file.close();
@@ -83,13 +142,19 @@ class Backend():
     
     
     def get_station_list(self):
+        '''
+        Returns the list of stations. Each station is represented by a tuple:
+        (<readable station name>, <station uri>).
+        @return: List of tuple, one tuple for each station with name and URI 
+        '''
         return self.stations
     
     
     def update(self):
         '''
-        Called when the controls of the radio change, i.e. station, volume, 
-        audio source switch.
+        Callback for control updates, i.e. change of station, volume or audio 
+        source. The actual effect of a control input is realized here.
+        Thread safe, requires controls object's lock
         '''
         # > lock
         self.controls.lock.acquire()
@@ -107,15 +172,28 @@ class Backend():
     
     # -------- Play -----------------------------------------------------------
     def switch_station(self, new_station):
+        '''
+        Switches to the given station. Switching to the same station as the 
+        current should be avoided.
+        Thread safe, requires "noise_lock" and "mpc_lock"
+        @param new_station: Index of the station to switch to. Station 0 is 
+        noise. Real stations start at 1.
+        '''
         print 'switching to station: ' + str(new_station)
+        # station 0 plays noise
         if new_station == 0:
             self.stop()
             # > lock
             self.noise_lock.acquire()
+            # kill already running process, should not be the case
+            if self.noise_process is not None:
+                self.noise_process.kill()
+            # start new process playing noise
             self.noise_process = sub.Popen(['play', 'radio-noise.ogg', 'repeat'])
             # < release
             self.noise_lock.release()
         else:
+            # stop running noise process
             if self.noise_process is not None:
                 # > lock
                 self.noise_lock.acquire()
@@ -125,21 +203,38 @@ class Backend():
                 self.noise_lock.release()
             # > lock
             self.mpc_lock.acquire()
+            # switch to new station
             sub.call(['mpc', 'play', str(new_station)], stdout=FNULL)
             # < release
             self.mpc_lock.release()
     
     
-    def play(self):
+    def play(self, lock_controls=True):
+        '''
+        Starts playing the current station, i.e. current station from controls
+        object.
+        Thread safe, requires controls object's lock
+        @param lock_controls: optional, when set to False will not lock 
+        controls, default is True
+        '''
         # > lock
-        self.controls.lock.acquire()
+        if lock_controls:
+            self.controls.lock.acquire()
+        
         self.switch_station(self.controls.station)
+        
         # < release
-        self.controls.lock.release()
+        if lock_controls:
+            self.controls.lock.release()
     
     
     # -------- State ----------------------------------------------------------
     def get_state(self):
+        '''
+        Get the volume and currently selected (not necessarily playing) station.
+        Calls thread safe methods, only.
+        @return: Dictionary with keys 'volume' and 'station'
+        '''
         volume = self.get_volume()
         station = self.get_current_station()
         return dict(volume=volume, station=station)
@@ -147,6 +242,10 @@ class Backend():
     
     # -------- Stop -----------------------------------------------------------
     def stop(self):
+        '''
+        Stops the radio.
+        Thread safe, requires "mpc_lock"
+        '''
         # > lock
         self.mpc_lock.acquire()
         sub.call(['mpc', 'stop'], stdout=FNULL)
@@ -156,6 +255,12 @@ class Backend():
     
     # -------- Volume ---------------------------------------------------------
     def set_volume(self, new_volume):
+        '''
+        Set new volume through amixer.
+        Thread safe, requires "amixer_lock"
+        @param new_volume: New volume in percent (0 -> silence, 100 -> as loud 
+        as it gets)
+        '''
         # > lock
         self.amixer_lock.acquire()
         sub.call(['amixer', '-c', '0', 'sset', 'Headphone', str(new_volume) + '%'], stdout=FNULL)
@@ -164,6 +269,11 @@ class Backend():
     
     
     def get_volume(self):
+        '''
+        Get the current volume through amixer.
+        Thread safe, requires "amixer_lock"
+        @return: Current volume in percent
+        '''
         # > lock
         self.amixer_lock.acquire()
         amixer = sub.check_output(['amixer', '-c', '0', 'sget', 'Headphone'])
@@ -175,6 +285,13 @@ class Backend():
     
     # -------- Audio Source ---------------------------------------------------
     def set_ext_audio_source(self, use_external):
+        '''
+        Switches between external audio source and internal. External means 
+        using the microphones playback for ouput with amixer.
+        Thread safe, requires "amixer_lock"
+        @param use_external: When True the audio source plugged to the 
+        microphone jack is looped through to be played, otherwise it is muted.
+        '''
         # > lock
         self.amixer_lock.acquire()
         if use_external:
@@ -182,13 +299,18 @@ class Backend():
             sub.call(['amixer', 'sset', 'Mic', 'Playback', 'unmute'])
         else:
             sub.call(['amixer', 'sset', 'Mic', 'mute'])
-            self.play()
+            self.play(lock_controls=False)
         # < release
         self.amixer_lock.release()
     
     
     # -------- Station --------------------------------------------------------
     def get_current_station(self):
+        '''
+        Get the currently playing station from mpc.
+        Thread safe, requires "mpc_lock"
+        @return: Currently playing station
+        '''
         self.mpc_lock.acquire()
         mpc = sub.check_output(['mpc', 'current'])
         station = mpc.strip()
---
+++ radiocontrol/control/utils.py
@@ -0,0 +1,77 @@
+import subprocess as sub
+import re
+
+PACKET_LOSS_REGEX = re.compile('(\d)%\spacket\sloss')
+URI_REGEX = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
+AD_HOC_CONF = [ 'iface wlan0 inet static\n',
+                '\taddress 192.168.1.222\n',
+                '\tnetmask 255.255.255.0\n',
+                '\twireless-channel 1\n',
+                '\twireless-essid PI-RADIO\n',
+                '\twireless-mode ad-hoc\n' ]
+
+
+def check_inet_connection():
+    '''
+    Tries to ping fff.org three times with an overall timeout of five seconds
+    and returns the percentaged packet loss. If there is no connection at all
+    100 is returned.
+    @return: Percentaged packet loss for three pings
+    '''
+    # try to quietly ping fff.org three times with a deadline of 5s
+    try:
+        ping_output = sub.check_output(['ping', '-q', '-c', '3', '-w', '5', 'fff.org'])
+    except sub.CalledProcessError:
+        return 100
+    # get packet loss in %
+    packet_loss = PACKET_LOSS_REGEX.findall(ping_output)
+    # when there is no connection at all "unknown host" is returned, so the
+    # regex won't find anything
+    if len(packet_loss) > 0:
+        packet_loss = int(packet_loss[0])
+    else:
+        packet_loss = 100
+    return packet_loss
+
+
+def resolve_m3u(m3u):
+    '''
+    Tries to resolve the given m3u by parsing its output and taking the first
+    URI. This URI is then returned.
+    @param m3u: A URI ending with .m3u that should be resolved
+    @raise ValueError: When resolving fails
+    @return: The first URI in the m3u
+    '''
+    m3u_data = sub.check_output(['curl', m3u])
+    uris = URI_REGEX.findall(m3u_data)
+    if len(uris) > 0:
+        return uris[0]
+    else:
+        raise ValueError()
+
+
+def setup_ad_hoc():
+    sub.call(['ifdown', 'wlan0'])
+    
+    ifaces_file = open('/etc/network/interfaces', 'r')
+    ifaces_data = ifaces_file.readlines()
+    print ifaces_data
+    
+    # remove old configuration
+    start_line_idx = ifaces_data.index('iface wlan0 inet static\n')
+    current_line = ifaces_data[start_line_idx]
+    while len(current_line.strip()) > 0:
+        ifaces_data.remove(current_line)
+        current_line = ifaces_data[start_line_idx]
+    
+    # add new ad-hoc configuration
+    offset = 0
+    for line in AD_HOC_CONF:
+        ifaces_data.insert(start_line_idx + offset, line)
+        offset += 1
+    
+    ifaces_file = open('/etc/network/interfaces', 'w')
+    ifaces_file.writelines(ifaces_data)
+    
+    sub.call(['ifup', 'wlan0'])
